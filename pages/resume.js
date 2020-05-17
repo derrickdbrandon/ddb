8c6ce6af392bd8a36625722ce47a769eec9411be
derrickdbrandon <derrickdbrandon@gmail.com>
2020-05-17 13:39:51 -0400

    add resume iframe component

---
+++ pages/resume.js
@@ -0,0 +1,8 @@
+export default function Resume() {
+  return (
+    <iframe
+      src="/derrick_brandon_resume.pdf"
+      style={{ position: 'absolute', height: '100%', width: '100%' }}
+    />
+  );
+}
