83b9da4c69ed05d5287561f162f6f3a0bcda6cfa
Derrick Brandon <derrickdbrandon@gmail.com>
2020-12-30 18:33:05 -0500

    apply static html changes to next app

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.js
@@ -0,0 +1,116 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Derrick D. Brandon</title>
+        <meta name="Derrick D. Brandon" content="Derrick D. Brandon" />
+        <meta
+          name="Description"
+          content="Derrick D. Brandon Software Engineer"
+        ></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <h1>Derrick D. Brandon</h1>
+        <h2>Brooklyn, NY</h2>
+        <p>
+          Fullstack developer currently full-time at{" "}
+          <a href="https://www.justvanilla.com/">Vanilla</a>.
+        </p>
+      </body>
+      <footer>
+        <small>
+          <a href="mailto:derrickdbrandon@gmail.com">email</a> /{" "}
+          <a href="https://github.com/derrickdbrandon">github</a> /{" "}
+          <a href="https://www.are.na/derrick-brandon">are.na</a>
+        </small>
+      </footer>
+
+      <style jsx global>{`
+        @font-face {
+          font-family: "AUTHENTICSans";
+          src: url("fonts/AUTHENTIC-Sans.otf") format("otf"),
+            url("fonts/AUTHENTIC-Sans.woff") format("woff"),
+            url("fonts/AUTHENTIC-Sans.woff2"), format("woff2");
+        }
+
+        body {
+          background-color: #9f8f92;
+          font-family: "AUTHENTICSans", "Arial", sans-serif;
+          font-size: 40px;
+          text-align: center;
+        }
+
+        h1,
+        h2 {
+          font-weight: normal;
+          line-height: 1em;
+          margin: 0 auto;
+        }
+
+        h1 {
+          -moz-transition: all 0.4s;
+          -webkit-transition: all 0.4s;
+          -o-transition: all 0.4s;
+          transition: all 0.4s;
+        }
+
+        h1:hover {
+          color: white;
+          cursor: pointer;
+          text-shadow: 0 0 20px #fff;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+          -moz-transition: all 0.4s;
+          -webkit-transition: all 0.4s;
+          -o-transition: all 0.4s;
+          transition: all 0.4s;
+        }
+
+        a:hover {
+          color: white;
+          text-shadow: 0 0 20px #fff;
+        }
+
+        p {
+          position: absolute;
+          left: 50%;
+          top: 44%;
+          -webkit-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+        }
+
+        footer {
+          position: absolute;
+          bottom: 0;
+          left: 50%;
+          -webkit-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+        }
+
+        @media only screen and (max-width: 630px) {
+          body {
+            font-size: 30px;
+          }
+        }
+
+        @media only screen and (max-width: 495px) {
+          body {
+            font-size: 24px;
+          }
+        }
+
+        @media only screen and (max-width: 420px) {
+          body {
+            font-size: 18px;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
